4442239131a8eebd088cb7146e93948ba91876d5
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-17 11:08:22 +0300

    Adding JSDoc and comments

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+/**
+ * @module xwindow
+ * @author Vyacheslav Aristov <vv.aristov@gmail.com>
+ */
 if(typeof window === 'undefined') {
+  // Calling via eval() does not allow the module bundler to extract jsdom
   const { JSDOM } = eval('require("jsdom")')
   const { window } = new JSDOM
   module.exports = window
